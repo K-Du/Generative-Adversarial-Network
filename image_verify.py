ba568c2176d7a5af6e297e7f156083bef6e19486
Kevin Du <kevinkldu@gmail.com>
2017-04-12 19:35:57 -0700

    Update image_verify.py

--- image_verify.py
+++ image_verify.py
@@ -5,11 +5,11 @@ import glob
 import os
 
 for i, image_name in enumerate(glob.glob(os.path.join(os.getcwd(), '*.jpg'))):
-  print i, image_name
-  with tf.Graph().as_default():
-    image_contents = tf.read_file(image_name)
-    image = tf.image.decode_jpeg(image_contents, channels=3)
-    init_op = tf.initialize_all_tables()
-    with tf.Session() as sess:
-      sess.run(init_op)
-      tmp = sess.run(image)
+    print i, image_name
+    with tf.Graph().as_default():
+        image_contents = tf.read_file(image_name)
+        image = tf.image.decode_jpeg(image_contents, channels=3)
+        init_op = tf.initialize_all_tables()
+        with tf.Session() as sess:
+            sess.run(init_op)
+            tmp = sess.run(image)
